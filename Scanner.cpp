9a4ab705454ab58aa0dc8079d8041d7c3b71e637
eagerbeaver04 <mike.markov04@gmail.com>
2024-05-02 19:40:24 +0300

    Add check for input data

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,6 +1,43 @@
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(int argc, char** argv)
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include "PortRange.h"
+
+int main(int argc, char* argv[])
 {
+    if (argc != 4)
+    {
+        std::cerr << "Incorrect number of input values\n";
+        return 1; 
+    }
+
+    PortRange port {argv[1], std::atoi(argv[2]), std::atoi(argv[3])};
+
+    if(!port.is_valid())
+    {
+        std::cerr << "Incorrect port range\n";
+        return 1;
+    }
+
+    int epollfd = epoll_create1(0);
+    if (epollfd == -1)
+    {
+        std::cerr << "Failed to create epoll instance" << std::endl;
+        return 1;
+    }
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLOUT; 
+    event.data.fd = -1;
+
+    std::vector<int> sockets;
+
+
     return 0;
 }
